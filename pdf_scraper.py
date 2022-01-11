24336c6e72b4ffa68cbf3c311b41c6521ba2e483
Jakub Krajewski <jakub.j.krajewski@gmail.com>
2022-01-11 12:54:40 +0000

    updated pdf_scraper

--- pdf_scraper.py
+++ pdf_scraper.py
@@ -10,63 +10,7 @@ def raw_data_from_tables(fis_pdf):
     :return: lists of rows with jumper data
     """
 
-    all_content = []
-
-    fis_pdf = fis_pdf + '.pdf'
-
-    # unpacking pdf
-    pdf_file = pdfplumber.open(fis_pdf)
-
-    page_1 = pdf_file.pages[0]
-    content_page_1 = page_1.extract_tables()
-
-    # for pdf that pdfplumber not see tables
-    if len(content_page_1) == 0:
-        print('PDF with no tables!')
-
-        page_2 = pdf_file.pages[1]
-
-        content_1 = page_1.extract_text().split('\n')[16:-2]
-        content_2 = page_2.extract_text().split('\n')[7:-2]
-
-        content = content_1 + content_2
-
-        # clear rows
-        raw_content = []
-        data_to_skip = ['Club', 'Rank', 'Name', 'Fini', 'Not ', 'Disq', 'Did ', 'Code']
-
-        for row in content:
-            line = row.split('\n')
-
-            if line[0][0:4] in data_to_skip:
-                continue
-            elif line[0] == 'Weather Information':
-                break
-
-            raw_content.append(line)
-
-        all_content = pdf_with_no_tables_scraper(raw_content)
-
-    # pdf with only 1 page
-    elif len(pdf_file.pages) == 1:
-        print('PDF with 1 page')
-        pass
-
-    # pdf with 2 pages
-    elif len(pdf_file.pages) == 2:
-        print('PDF with 2 pages')
-        pass
-
-    # pdf with 3 pages
-    elif len(pdf_file.pages) == 3 and len(page_1.extract_tables()) > 0:
-        print('PDF with 3 pages')
-        pass
-
-    else:
-        print('PDF with more then 3 pages')
-        pass
-
-    return all_content
+    
 
 
 def create_csv_file(pdf_name, extracted_data):
@@ -74,7 +18,4 @@ def create_csv_file(pdf_name, extracted_data):
     Function creates csv file with data extracted from pdf, also creates same name as pdf name.
     """
 
-    file_name = pdf_name + '.csv'
 
-    with open(file_name, 'w') as fh:
-        fh.writelines(extracted_data)
